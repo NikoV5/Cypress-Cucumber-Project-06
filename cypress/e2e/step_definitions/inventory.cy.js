3b790e0cc793ae1e527a87a547b92e8d9ad0a28d
NikoV5 <NikoVourtsis7@gmail.com>
2024-06-25 15:50:21 -0500

    Added Updated Files

--- cypress/e2e/step_definitions/inventory.cy.js
+++ cypress/e2e/step_definitions/inventory.cy.js
@@ -1,3 +1,4 @@
+/// <reference types="cypress" />
 const { default: InventoryPage } = require("../pages/InventoryPage");
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
